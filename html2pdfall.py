6abb448b87fe4cfd54534f3495214512e172b42c
dmitry <dnluzin@gmail.com>
2023-04-02 12:06:44 +0300

    write json to file

--- html2pdfall.py
+++ html2pdfall.py
@@ -11,27 +11,30 @@ tags = soup.find_all("loc")
 
 all_uri_in_xml_sitemap = []
 for tag in tags:
-	all_uri_in_xml_sitemap.append(tag.text)
+  all_uri_in_xml_sitemap.append(tag.text)
 
 posts_params = {}
-posts_params_index = 0
 images = [".jpg", ".jpeg", ".png", ".webp"]
 for uri in all_uri_in_xml_sitemap:
-	if any(image in uri for image in images):
-		# do not include links to images 
-		pass
-	else:
-		uri_parts = uri.split('/')
-		if uri_parts[3]:
-			slug = uri_parts[3]
-			print(posts_params_index)
-			print(slug)
+  if any(image in uri for image in images):
+    # do not include links to images 
+    pass
+  else:
+    uri_parts = uri.split('/')
+    if uri_parts[3]:
+      slug = uri_parts[3]
 
-			converter.convert(uri, './pdf/', f'{slug}.pdf')
+      converter.convert(uri, './pdf/', f'{slug}.pdf')
 
-			request = requests.get(config.SITE_URL + config.API_URN + slug, headers=config.HEADERS)
-			posts_json = json.loads(request.text)
-			title = posts_json[0]['title']['rendered']
-			print(title) 
-			posts_params[posts_params_index] = {'slug': slug, 'title': title, 'url': uri}
-			posts_params_index += 1
+      request = requests.get(config.SITE_URL + config.API_URN + slug, headers=config.HEADERS)
+      posts_json = json.loads(request.text)
+      title = posts_json[0]['title']['rendered']
+      print(title) 
+      posts_params[slug] = {'title': title, 'url': uri}
+
+posts_params_reversed = {}
+for key in reversed(posts_params):
+  posts_params_reversed[key] = posts_params[key]
+
+with open('posts_params.json', 'w') as file:
+  json.dump(posts_params_reversed, file, indent=4, ensure_ascii=False)
